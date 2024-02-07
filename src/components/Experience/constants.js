57a7dc0069e122b39b24a80794e9d73f9b3a6cfa
Yandapalli Vivek <vivekyandapalli@gmail.com>
2024-02-07 18:45:59 +0530

    added tech stack

--- src/components/Experience/constants.js
+++ src/components/Experience/constants.js
@@ -1,70 +1,50 @@
 export const experience = [
-    {
-        company: "Luxoft",
-        id: "luxoft",
-        from: "07/2022",
-        description: [
-            "Developed indispensable data science tools streamlining feature creation and automating data scraping, eliminating repetitive tasks for enhanced efficiency.",
-            "Acting as a bridge between users and development teams to gather requirements.",
-            "Utilizing micro frontend architecture to create modular and scalable applications using Webpack and Angular.",
-            "Balancing technical expertise with leadership skills to guide the development process."
-        ],
-        techStack: [
-            "Angular",
-            "PrimeNG",
-            "SCSS",
-            "Micro Frontend"
-        ]
-    },
-    {
-        company: "ADP",
-        id: "adp",
-        from: "05/2021",
-        to: "06/2022",
-        description: [
-            "Spearheaded UI team, overseeing development of a portal allowing service teams to autonomously create and integrate their micro front-end applications.",
-            "Pioneered a consolidated platform offering users a seamless one-stop solution for diverse service requests.",
-            "Utilized React to develop these micro front-end applications, ensuring robust functionality and user-friendly interfaces.",
-        ],
-        techStack: [
-            "React",
-            "SCSS",
-            "Micro Frontend"
-        ]
-    },
-    {
-        company: "Gramener",
-        id: "gramener",
-        from: "06/2018",
-        to: "05/2021",
-        description: [
-            "Lead UI team in conceptualizing and constructing visualization applications, leveraging React, Python, and d3.js.",
-            "Empowered clients to comprehend complex data effortlessly through interactive visualizations, enhancing data-driven decision-making.",
-            "Accelerated data understanding by providing users with intuitive visual representations, minimizing the need to navigate raw data.",
-        ],
-        techStack: [
-            "Python",
-            "React",
-            "Material UI",
-            "SCSS",
-            "D3"
-        ]
-    },
-    {
-        company: "Eram Infotech",
-        id: "eram-infotech",
-        from: "08/2016",
-        to: "05/2018",
-        description: [
-           "Developed a customized travel and accounting solution using Python, HTML, JavaScript, and CSS within the Odoo open source ERP and CRM framework.",
-           "Enabled travel agencies to seamlessly book tickets and manage accounting processes through the tailored application.",
-           "Utilized Odoo's flexibility to create a user-friendly interface, enhancing booking and accounting workflows for clients."
-        ],
-        techStack: [
-            "Python",
-            "Odoo",
-            "HTML",
-            "CSS"
-        ]
-    },
-]
+  {
+    company: "Luxoft",
+    id: "luxoft",
+    from: "07/2022",
+    description: [
+      "Developed indispensable data science tools streamlining feature creation and automating data scraping, eliminating repetitive tasks for enhanced efficiency.",
+      "Acting as a bridge between users and development teams to gather requirements.",
+      "Utilizing Micro Frontend architecture to create modular applications using Webpack, Angular and data fetching using GraphQL",
+      "Balancing technical expertise with leadership skills to guide the development process.",
+    ],
+    techStack: ["Angular", "PrimeNG", "SCSS", "GraphQL", "Micro Frontend"],
+  },
+  {
+    company: "ADP",
+    id: "adp",
+    from: "05/2021",
+    to: "06/2022",
+    description: [
+      "Spearheaded UI team, overseeing development of a portal allowing service teams to autonomously create and integrate their micro front-end applications.",
+      "Pioneered a consolidated platform offering users a seamless one-stop solution for diverse service requests.",
+      "Utilized React to develop these micro front-end applications, ensuring robust functionality and user-friendly interfaces.",
+    ],
+    techStack: ["React", "SCSS", "Micro Frontend"],
+  },
+  {
+    company: "Gramener",
+    id: "gramener",
+    from: "06/2018",
+    to: "05/2021",
+    description: [
+      "Lead UI team in conceptualizing and constructing visualization applications, leveraging React, Python, and d3.js.",
+      "Empowered clients to comprehend complex data effortlessly through interactive visualizations, enhancing data-driven decision-making.",
+      "Accelerated data understanding by providing users with intuitive visual representations, minimizing the need to navigate raw data.",
+    ],
+    techStack: ["Python", "React", "Material UI", "SCSS", "D3"],
+  },
+  {
+    company: "Eram Infotech",
+    id: "eram-infotech",
+    from: "08/2016",
+    to: "05/2018",
+    description: [
+      "Developed a customized travel and accounting solution using Python, HTML, JavaScript, and CSS within the Odoo open source ERP and CRM framework.",
+      "Enabled travel agencies to seamlessly book tickets and manage accounting processes through the tailored application.",
+      "Utilized Odoo's flexibility to create a user-friendly interface, enhancing booking and accounting workflows for clients.",
+    ],
+    techStack: ["Python", "Odoo", "HTML", "CSS"],
+  },
+];
